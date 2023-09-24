14e52f999f9c1d473cfe2fabee2997d049cd45ff
Allan Viray <allang.v.dev@gmail.com>
2023-09-24 17:30:12 -0500

    Add logic to simulate dice roll

--- src/JavaDice.java
+++ src/JavaDice.java
@@ -1,5 +1,17 @@
+import java.util.Random;
+
 public class JavaDice {
     public static void main(String[] args) {
-        System.out.println("Hello Java Dice!");
+        Random randomizedNumber = new Random();
+        int x = randomizedNumber.nextInt(6) + 1;
+        if (x < 1) {
+            System.out.println("***********************************");
+            System.out.println("Invalid roll. Please roll again. =)");
+            System.out.println("***********************************");
+        }
+
+        System.out.println("***********************************");
+        System.out.println("You rolled a: " + x);
+        System.out.println("***********************************");
     }
 }
