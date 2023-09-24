081cd4e23204efe91719c2d38e67691a210f8be6
Allan Viray <allang.v.dev@gmail.com>
2023-09-24 17:01:20 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java Dice (currently under construction)
+
+<em>Java Dice</em> is a Java program that simulates a dice roll.
---
+++ src/JavaDice.java
@@ -0,0 +1,5 @@
+public class JavaDice {
+    public static void main(String[] args) {
+        System.out.println("Hello Java Dice!");
+    }
+}
