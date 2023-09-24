cd5c3eba4cd17b6fb6a34e69db3e6a08f5e2c86c
Allan Viray <allang.v.dev@gmail.com>
2023-09-24 17:47:58 -0500

    Add comments

--- src/JavaDice.java
+++ src/JavaDice.java
@@ -2,7 +2,23 @@ import java.util.Random;
 
 public class JavaDice {
     public static void main(String[] args) {
+        // *** About the Random class (part of Java util package) ***
+        // - Java's Random class generates a pseudorandom number.
+
+        // - Use new instance of Random class to generate a pseudorandom number.
         Random randomizedNumber = new Random();
+
+        // *** About Random's .nextInt(int n) method ***
+        // - Random.nextInt(int n) will get a random number between a range of 0 and the int n number that we specify.
+        // - Note that the int n argument passed into nextInt(int n) is exclusive. This means that it will be
+        //   excluded from the range of numbers. In other words, if we specify nextInt(10), the randomized number
+        //   will result in being any number between 0 and 9--obviously not including 10.
+        // - Therefore, if we wanted a random number between 0 and 10, we would have to write something
+        //   like nextInt(10) + 1.
+
+        // - Get one of 6 integers using Random.nextInt(int n) method.
+        // - Random class's nextInt(int n) method is used to get a random number between a range of 0(inclusive)
+        //   and the argument int n(exclusive), that we specify.
         int x = randomizedNumber.nextInt(6) + 1;
         if (x < 1) {
             System.out.println("***********************************");
